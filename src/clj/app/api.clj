1297d867c8308279c902d8863d76dddd2e10ee1a
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-10-11 23:15:39 +0100

    OBR-1 added handler and route for updating password

--- src/clj/app/api.clj
+++ src/clj/app/api.clj
@@ -26,6 +26,18 @@
       {:status 401
        :body   (ex-data e)})))
 
+(defn change-user-password
+  "Web handler to change a user's password"
+  [{{{:keys [username old-password new-password]} :query} :parameters} :as ctx]
+  (try
+    (auth/authenticate-user @auth/user-database username old-password)
+    {:status 200
+     :body (auth/change-user-password @auth/user-database username new-password)}
+    (catch Exception e
+      {:status 401
+       :body   (ex-data e)})))
+
+
 (def routes
   ["" {:coercion reitit.coercion.spec/coercion}
    ["/user" {:get  {:summary    "Authenticates a user in the database, returning their details"
@@ -36,4 +48,10 @@
                     :parameters {:body {:username      string?
                                         :password      string?
                                         (ds/opt :role) keyword?}}
-                    :handler    create-user-handler}}]])
+                    :handler    create-user-handler}}
+    ["/password" {:put {:summary    "Updates a user's password."
+                         :parameters {:body {:username      string?
+                                             :old-password  string?
+                                             :new-password  string?
+                                             (ds/opt :role) keyword?}}
+                         :handler    create-user-handler}}]]])
