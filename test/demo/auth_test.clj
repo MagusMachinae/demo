de8493b13b285d290847248d5ac5b825b242d4d8
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-10-13 16:58:22 +0100

    OBR-1 password rules test flow

--- test/demo/auth_test.clj
+++ test/demo/auth_test.clj
@@ -1,4 +1,20 @@
 (ns demo.api-test
-  (:require [clojure.test :refer [deftest is testing]]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [demo.backend.auth :as auth]))
 
-(deftest)
+
+(deftest password-rules
+  (testing "all rules are applied individually and in composition"
+    (is (= #{:password.error/too-short}
+           (auth/password-rules "ABa$1")))
+    (is (= #{:password.error/missing-special-character}
+           (auth/password-rules "ABacdasw1")))
+    (is (= #{:password.error/missing-lowercase}
+           (auth/password-rules "ABCD£!£$%")))
+    (is (= #{:password.error/missing-uppercase}
+           (auth/password-rules "abcd££%$£")))
+    (is (= #{:password.error/too-short
+             :password.error/missing-uppercase
+             :password.error/missing-lowercase
+             :password.error/missing-special-character}
+           (auth/password-rules "")))))
