67b4be70578942fbc6b794a245904f1dda0cb23f
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-10-15 21:52:52 +0100

    OBR-1 lay out api tests

--- test/demo/api_test.clj
+++ test/demo/api_test.clj
@@ -1,1 +1,15 @@
-(ns demo.api-test)
+(ns demo.api-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [demo.backend.api :as api]))
+
+(def valid-request {:})
+
+(deftest user-handler
+  (testing "400 OK"
+    (is (= {}))))
+
+(deftest authenticate-user-handler
+  (testing "400 OK"))
+
+(deftest change-user-password-handler
+  (testing "400 OK"))
