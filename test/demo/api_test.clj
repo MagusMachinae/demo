9dc240744e8730a3a92a885fdb9d7d6f26525848
MagusMachinae <Edward.Hughes1911@gmail.com>
2021-10-17 16:18:13 +0100

    OBR-1 test flow for auth handler

--- test/demo/api_test.clj
+++ test/demo/api_test.clj
@@ -13,7 +13,19 @@
                                :password "for-now"}}))
 
 (deftest authenticate-user-handler
-  (testing "400 OK"))
+  (testing "valid GET request to /api/authenticate returns 200 OK and user"
+    (let  [res (api/authenticate-user-handler test-db
+                (mock/request :get "/api/user?id=i-exist&password=for-now"))]
+      (is (= 200
+             (:status res)))
+      (is (= {:id       "i-exist"
+              :role :user}
+             (:result (:body res))))))
+  (testing "invalid GET request to /api/authenticate throws 400")
+    (let [res (api/authenticate-user-handler test-db
+               (mock/request :get "/api/user?id=i-don't-exist&password=for-now"))]
+      (is (= 400
+             (:status res)))))
 
 (deftest change-user-password-handler
   (testing "400 OK"))
